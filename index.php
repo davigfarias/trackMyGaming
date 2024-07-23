46c9fb4e9610f6a3e22c726882db910eb0398c4f
Dave Farias <seto.davigf@gmail.com>
2024-07-22 23:45:32 -0300

    Initial Stage of the Project

---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="../includes/css/home.style.css">
+    <title>Track My Gaming! &#153</title>
+</head>
+
+<body>
+    <main class="container">
+        <section class="text-center my-3">
+            <h1>Track My Gaming! &#153</h1>
+        </section>
+        <section>
+            <div class="row text-center">
+                <div class="col">
+                    <a href="">#</a>
+                    <a href="">A</a>
+                    <a href="">B</a>
+                    <a href="">C</a>
+                    <a href="">D</a>
+                    <a href="">E</a>
+                    <a href="">F</a>
+                    <a href="">G</a>
+                    <a href="">H</a>
+                    <a href="">I</a>
+                    <a href="">J</a>
+                    <a href="">K</a>
+                    <a href="">L</a>
+                    <a href="">M</a>
+                    <a href="">N</a>
+                    <a href="">O</a>
+                    <a href="">P</a>
+                    <a href="">Q</a>
+                    <a href="">R</a>
+                    <a href="">S</a>
+                    <a href="">T</a>
+                    <a href="">U</a>
+                    <a href="">V</a>
+                    <a href="">X</a>
+                    <a href="">W</a>
+                    <a href="">Y</a>
+                    <a href="">Z</a>
+                </div>
+            </div>
+        </section>
+
+        <section>
+
+        </section>
+    </main>
+</body>
+
+</html>
